4c9ff2cf504b3ef791d19f12d674d175ce2115c9
Rodolfo Vergara <vergarayepes@hotmail.com>
2024-11-15 14:45:50 -0500

    Mi primer Hola Python

---
+++ 00_hello.py
@@ -0,0 +1,13 @@
+# Hola mundo // Esto es un comentario 
+print("Hola Python")
+print('Hola Python')
+"""
+Esto es un comentario 
+en variaslineas
+"""
+#Consulta el tipo de dato 
+print(type("Hola mundo "))  # tipo 'str'
+print(type(12))  # tipo 'int'
+print(type(1.2))  # tipo 'float'
+print(type(1 + 2j))  # tipo 'Complex'
+print(type(True))  # tipo 'bool'
