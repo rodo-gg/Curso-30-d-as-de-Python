549c298f96acaac46005b052ce84d443413c5217
rodo-gg <vergarayepes@hotmail.com>
2024-11-20 16:28:14 -0500

    Clase de operadores

---
+++ 02_operators.py
@@ -0,0 +1,40 @@
+###Operadores Aritméticos###
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(10 % 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 * 2 + 10 / 2 // 8)
+
+print("Hola" + "Python")
+print("Hola" + str(5))
+print("Hola" * 3)
+print("Hola" * (3 ** 3))
+
+my_float = 2.5
+print("Hola" * int(my_float))
+
+###Operadores Comparativos###
+
+print(3 < 4)
+print(3 > 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" < "Hola")
+print("Hola" > "Hola")
+print("Hola" >= "hola") #Ordenación alfabeticamente por ASCII
+print("Hola" <= "Hola")
+print("Hola" == "Hola")
+print("Hola" != "Hola")
+
+###Operadores Comparativos###
+
+print(3 > 4 and "hola mundo" == "hola mundo")
+print(3 > 4 or "hola mundo" < "hola mundo")
+print(not(3 > 4))
